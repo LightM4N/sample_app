824ce272e962a0134b2015f580ab0de72f7981f3
LightM4N <arthurnurutdinov@gmail.com>
2015-11-03 22:16:24 +0300

    сделаны новости, добавлены подписчики

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,16 @@
 class User < ActiveRecord::Base
 	has_many :microposts, dependent: :destroy
+	has_many :relationships, foreign_key: "follower_id", dependent: :destroy
+	has_many :followed_users, through: :relationships, source: :followed
 	before_save {self.email=email.downcase}
 	before_create :create_remember_token
 	validates :name, presence: true, length: {maximum: 50}
+
+	has_many :reverse_relationships, foreign_key: "followed_id", 
+										class_name: "Relationship",
+										dependent: :destroy
+
+	has_many :followers, through: :reverse_relationships, source: :follower
 	
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 	validates :email, presence: true, format: {with: VALID_EMAIL_REGEX},
@@ -15,14 +23,25 @@ class User < ActiveRecord::Base
 		SecureRandom.urlsafe_base64
 	end
 
-	def User.encrypt (token)
+	def User.encrypt(token)
 		Digest::SHA1.hexdigest(token.to_s)
 	end
 
 	def feed
-		#пред реализация, остальное потом
-		Micropost.where("user_id = ?", id)
+		Micropost.from_users_followed_by(self)
 	end
+
+	def following?(other_user)
+		relationships.find_by(followed_id: other_user.id)
+	end
+
+	def follow!(other_user)
+		relationships.create!(followed_id: other_user.id)
+	end
+
+ def unfollow!(other_user)
+    relationships.find_by(followed_id: other_user.id).destroy!
+  end
 
 	private
 
